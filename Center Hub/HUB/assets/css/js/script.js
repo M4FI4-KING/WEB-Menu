25392aa0298b4ca7eeb7b7b71fb0df9e3a619130
M4FI4-KING <vimashagimhan@gmail.com>
2025-10-02 22:12:17 +0530

    Delete Center Hub directory

--- Center Hub/HUB/assets/css/js/script.js
+++
@@ -1,3 +0,0 @@
-// Central Command Hub - JavaScript File
-// This file is currently empty, designed for a purely navigation-based hub.
-// Add future dynamic elements or terminal effects here if needed.
--- Center Hub/ONLINE-CLASS/assets/js/script.js
+++
@@ -1,2 +0,0 @@
-// Academic Data Stream - JavaScript File
-// Add interactive elements, quizzes, or terminal animations here if needed.
